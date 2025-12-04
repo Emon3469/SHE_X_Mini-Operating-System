e07450a129716bed6468ff9eaee6ed53242cb030
Himel <himelhasan1215@gmail.com>
2025-12-05 00:46:14 +0600

    Exceptions: replace comma operator with separate printhex calls in alignment_check_fault

--- kernal/hal/inthandling.c
+++ kernal/hal/inthandling.c
@@ -290,7 +290,7 @@ void fpu_fault  (uint32_t cs, uint32_t  eip, uint32_t flags)  {
 void alignment_check_fault (uint32_t eip,uint32_t cs,uint32_t flags,uint32_t err)  {
 
 	monitor_puts ("Alignment check :");
-	printhex(cs);printhex(eip);printhex(flags),printhex(err);
+	printhex(cs);printhex(eip);printhex(flags);printhex(err);
 	for (;;);
 }
 //! machine check
